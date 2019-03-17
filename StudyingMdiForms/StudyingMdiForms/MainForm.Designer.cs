5be8f2959e5d7fbaefc12065eb0207734b1e68b0
sorhan <saitorhan@outlook.com>
2019-03-18 02:23:38 +0300

    mdi form

---
+++ StudyingMdiForms/StudyingMdiForms/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace StudyingMdiForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.form1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.form2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.form3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.düzenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yatayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dikeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.döşemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.form1ToolStripMenuItem,
+            this.form2ToolStripMenuItem,
+            this.form3ToolStripMenuItem,
+            this.düzenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1079, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // form1ToolStripMenuItem
+            // 
+            this.form1ToolStripMenuItem.Name = "form1ToolStripMenuItem";
+            this.form1ToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.form1ToolStripMenuItem.Text = "Form1";
+            this.form1ToolStripMenuItem.Click += new System.EventHandler(this.form1ToolStripMenuItem_Click);
+            // 
+            // form2ToolStripMenuItem
+            // 
+            this.form2ToolStripMenuItem.Name = "form2ToolStripMenuItem";
+            this.form2ToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.form2ToolStripMenuItem.Text = "Form2";
+            this.form2ToolStripMenuItem.Click += new System.EventHandler(this.form2ToolStripMenuItem_Click);
+            // 
+            // form3ToolStripMenuItem
+            // 
+            this.form3ToolStripMenuItem.Name = "form3ToolStripMenuItem";
+            this.form3ToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.form3ToolStripMenuItem.Text = "Form3";
+            this.form3ToolStripMenuItem.Click += new System.EventHandler(this.form3ToolStripMenuItem_Click);
+            // 
+            // düzenToolStripMenuItem
+            // 
+            this.düzenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yatayToolStripMenuItem,
+            this.dikeyToolStripMenuItem,
+            this.döşemeToolStripMenuItem});
+            this.düzenToolStripMenuItem.Name = "düzenToolStripMenuItem";
+            this.düzenToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.düzenToolStripMenuItem.Text = "Düzen";
+            // 
+            // yatayToolStripMenuItem
+            // 
+            this.yatayToolStripMenuItem.Name = "yatayToolStripMenuItem";
+            this.yatayToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.yatayToolStripMenuItem.Text = "Yatay";
+            this.yatayToolStripMenuItem.Click += new System.EventHandler(this.yatayToolStripMenuItem_Click);
+            // 
+            // dikeyToolStripMenuItem
+            // 
+            this.dikeyToolStripMenuItem.Name = "dikeyToolStripMenuItem";
+            this.dikeyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.dikeyToolStripMenuItem.Text = "Dikey";
+            this.dikeyToolStripMenuItem.Click += new System.EventHandler(this.dikeyToolStripMenuItem_Click);
+            // 
+            // döşemeToolStripMenuItem
+            // 
+            this.döşemeToolStripMenuItem.Name = "döşemeToolStripMenuItem";
+            this.döşemeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.döşemeToolStripMenuItem.Text = "Döşeme";
+            this.döşemeToolStripMenuItem.Click += new System.EventHandler(this.döşemeToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1079, 31);
+            this.tabControl1.TabIndex = 2;
+            this.tabControl1.Visible = false;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            this.tabControl1.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.tabControl1_ControlAdded);
+            this.tabControl1.ControlRemoved += new System.Windows.Forms.ControlEventHandler(this.tabControl1_ControlRemoved);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 723);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.MdiChildActivate += new System.EventHandler(this.MainForm_MdiChildActivate);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem form1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem form2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem form3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem düzenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yatayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dikeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem döşemeToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ StudyingMdiForms/StudyingMdiForms/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyingMdiForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void form1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form1 = new Form1();
+            ShowForm( ref form1);
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form form = sender as Form;
+
+            TabPage deleteTabPage = null;
+
+            foreach (TabPage tabPage in tabControl1.TabPages)
+            {
+                if (tabPage.Text == form.Text)
+                {
+                    deleteTabPage = tabPage;
+                    break;
+                }
+            }
+
+            if (deleteTabPage != null)
+                tabControl1.TabPages.Remove(deleteTabPage);
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form2 = new Form2();
+            ShowForm(ref form2);
+
+        }
+
+        private void form3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form form3 = new Form3();
+            ShowForm(ref form3);
+
+        }
+
+        private void tabControl1_ControlAdded(object sender, ControlEventArgs e)
+        {
+            tabControl1.Visible = tabControl1.TabPages.Count > 0;
+        }
+
+        private void MainForm_MdiChildActivate(object sender, EventArgs e)
+        {
+            Form activeMdiChild = this.ActiveMdiChild;
+
+            foreach (TabPage tabPage in tabControl1.TabPages)
+            {
+                if (activeMdiChild.Text == tabPage.Text)
+                {
+                    tabControl1.SelectedTab = tabPage;
+                    break;
+                }
+            }
+        }
+
+        private void tabControl1_ControlRemoved(object sender, ControlEventArgs e)
+        {
+            tabControl1.Visible = tabControl1.TabPages.Count > 1;
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TabPage selectedTab = tabControl1.SelectedTab;
+            if (selectedTab == null)
+            {
+                return;
+            }
+
+            foreach (Form mdiChild in this.MdiChildren)
+            {
+                if (mdiChild.Text == selectedTab.Text)
+                {
+                    mdiChild.BringToFront();
+                    break;
+                }
+            }
+        }
+
+        private void yatayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void dikeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void döşemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        void ShowForm(ref Form form)
+        {
+            Form f = form;
+            Form tempForm = null;
+
+            foreach (Form child in this.MdiChildren)
+            {
+                if (child.Text == f.Text)
+                {
+                    tempForm = child;
+                    break;
+                }
+            }
+            
+            if (tempForm != null)
+            {
+                tempForm.BringToFront();
+            }
+            else
+            {
+                f.MdiParent = this;
+                f.Show();
+                f.FormClosed += Form1_FormClosed;
+
+                TabPage tabPage = new TabPage();
+                tabPage.Text = f.Text;
+                tabPage.Parent = tabControl1;
+            }
+            
+        }
+    }
+}
